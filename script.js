d8affccfb1722419a90a724ef107ea2e335e1605
Chotato <77442976+Chotato@users.noreply.github.com>
2025-07-02 16:19:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('.nav-links a');
+    links.forEach(link => {
+        if (link.href === window.location.href) {
+            link.classList.add('active');
+        }
+    });
+});
